37c824d67f08d60fa85039cf24079c8329c65eed
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-11-21 16:55:32 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,102 @@
+<html>
+	<head>
+		<style> 
+      .maincontainer {
+  width: 960px;
+  length:400px;
+  margin-top: 30px;
+  background-color: white;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.link-spanner{
+  position:absolute; 
+  width: 160px;
+  height: 100px;
+  top:0;
+  left: 0;
+  z-index: 1;
+}
+
+nav li {
+  font-family: 'Hind', serif;
+  display: inline;
+  font-size: 20px;
+  margin-left: 20px;
+}
+
+nav {
+  background-color: white;
+  width: 100%;
+  padding-top: 10px;
+  padding-bottom: 8px;
+}
+
+.titleblock {
+  border: solid #ffcccc 8px;
+  padding: 16px;
+  font-family: 'Lora', serif;
+  font-size: 38px;
+  width: 160px;
+  text-align: center;
+  margin: 25px;
+  color: gray;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: white;
+  font-weight: bold;
+  position: relative;
+}
+
+a {
+  color: black;
+  text-decoration: none;
+}
+
+a:hover {
+  color: #ffcccc;
+}
+
+body {
+  background-image: url("http://us.123rf.com/450wm/homodans/homodans1511/homodans151100018/47845946-doodle-flower-pattern-black-and-white.jpg?ver=6")
+}
+
+.abouttext {
+  font-family: 'Lora', serif;
+  font-size: 15px;
+  width: 800px;
+  text-align: left;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 50px;
+  padding-bottom: 50px;
+  line-height: 20px; 
+}
+      </style>
+	</head>
+  <head>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Hind">
+
+  <body>
+    <div class="maincontainer">
+      <nav>
+        <ul>
+          <li><a href=>SHOP</a></li>
+          <li><a href=>OCCASION</a></li>
+          <li><a href=>WANT TO SELL ON FLOOM</a></li>
+          <li><a href=>MERCH</a></li>
+        </ul>
+      </nav>
+      <div class="link-spanner titleblock">FLOOM<a href="https://s.codepen.io/htmlahoymichelle/debug/KzZxrp>"><span class="link-spanner"></span></a>
+      </div>
+      <div class="abouttext">This is your one-stop shop for all things flowers. Learn about plant varieties, view beautiful photographs of your favorite varieties, and create made-to-order arrangements with over 2,000 varieties. Our expertise ranges from your favorite traditional
+        picks to stunning rare specimin.
+        <br></br>
+        Flower Factory started out as the vision of avid gardener and greenspace artitect Violet Dandelion and blossomed into a business where people are connected to the beauty of flowers.
+      </div>
+    </div>
+  </body>
+
+</html>
